3fd35bee3cde3cc5a9cb54676a541b96b2d7d030
Herrick Wolber <wolbereric@yahoo.fr>
2019-01-03 16:43:26 +0100

    Ajout d'un projet de base iOS & Android

---
+++ ios files/ELIM project/Helpers, Keys & Statics/Keys.swift
@@ -0,0 +1,14 @@
+//
+//  Keys.swift
+//  ELIM project
+//
+//  Created by Herrick Wolber on 03/01/2019.
+//  Copyright © 2019 Herrick Wolber. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Keys: NSObject {
+
+}

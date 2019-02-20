99d2444ddfe59d458ef885b2bf3572b45afae01c
Rico <wolbereric@yahoo.fr>
2019-02-20 09:57:57 +0100

    Update HWMapsFragment.java

--- app/src/main/java/herrick/fr/advisor/Fragments/HWMapsFragment.java
+++ app/src/main/java/herrick/fr/advisor/Fragments/HWMapsFragment.java
@@ -1,31 +1,23 @@
 package herrick.fr.advisor.Fragments;
 
-
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.FindCallback;
 import com.parse.LogInCallback;
@@ -36,6 +28,7 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,257 +38,139 @@ import java.util.List;
 
 import herrick.fr.advisor.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class HWMapsFragment extends Fragment {
-    private ArrayList<ParseObject> myDataset = new ArrayList<>();
+    public HWMapsFragment() {}
 
-    private RecyclerView.Adapter mAdapter;
-    public static ArrayList<ParseObject> getAllArticlesWithLabel= new ArrayList<>();
-
-    public static String description="";
-    public static String description2="";
-
-    public HWMapsFragment() {
-        // Required empty public constructor
-    }
+    private final String TAG = "MapsFragment";
+    private ArrayList<ElimPins> elimPins = new ArrayList<>();
+    Double centerLat = 43.614922; Double centerLng = 7.071741;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_hwmaps, container, false);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg);
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onMapReady(GoogleMap mMap) {
+            public void onMapReady(final GoogleMap mMap) {
+
                 mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-                mMap.clear();
-
-
-
-
-
-
+                mMap.clear(); //clear old markers
 
                 ParseUser.becomeInBackground("r:963a9325f0de97c4fc1a29524053a2c7", new LogInCallback() {
                     @Override
                     public void done(ParseUser user, ParseException e) {
                         if (e == null) {
-                            final ParseQuery<ParseObject> query = ParseQuery.getQuery("UserProducts").addDescendingOrder("createdAt");
+                            final ParseQuery<ParseObject> query = ParseQuery.getQuery("UserProducts").addDescendingOrder("updatedAt");
                             query.whereEqualTo("user", user);
-
-                            query.setLimit(1);
 
                             query.findInBackground(new FindCallback<ParseObject>() {
 
                                 public void done(List<ParseObject> objects, ParseException ex) {
                                     if (ex == null) {
+                                        Log.i(TAG, objects.size()+"");
+                                        for (ParseObject obj : objects) {
+                                            // Google VISION results
+                                            JSONArray label = obj.getJSONArray("labelResults");
+                                            ParseFile file = obj.getParseFile("photoIn");
+                                            ParseGeoPoint geo = obj.getParseGeoPoint("positionAssocie");
 
-
-                                        JSONArray label = objects.get(0).getJSONArray("labelResults");
-
-
-                                        try {
-                                            description=label.getJSONObject(0).getString("description");
-                                            description2=label.getJSONObject(1).getString("description");
-
-                                        } catch (JSONException e1) {
-                                            e1.printStackTrace();
+                                            // Afficher seulement ceux avec une position
+                                            if (geo != null) {
+                                                try {
+                                                    if ( label.length() > 0) {
+                                                        String description = label.getJSONObject(0).getString("description");
+                                                        elimPins.add(new ElimPins(description, geo, file));
+                                                    }
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
                                         }
-                                         Toast.makeText(getActivity(),"descri1"+description+"descri2"+description2,Toast.LENGTH_LONG).show();
-                                        Log.d("maps", "Retrieved " + objects.size() + " objects");
+
+                                        fetchPinsData(mMap);
+
                                     } else {
-                                        Log.d("maps", "Error: " + ex.getMessage());
+                                        Log.d(TAG, "Error ("+ex.getCode()+") : " + ex.getMessage());
+                                        Toast.makeText(getContext(), "Error ("+ex.getCode()+") : " + ex.getMessage(), Toast.LENGTH_LONG).show();
                                     }
                                 }
                             });
 
 
-                            final ParseQuery<ParseObject> query1 = ParseQuery.getQuery("UserProducts").addDescendingOrder("updatedAt");
 
-
-
-
-
-                            query1.findInBackground(new FindCallback<ParseObject>() {
-
-                                public void done(List<ParseObject> objects, ParseException ex) {
-                                    getAllArticlesWithLabel.clear();
-                                    myDataset.clear();
-
-                                    if (ex == null) {
-
-
-                                        int a = objects.size();
-                                        for (int i=0; i<a; i++)
-                                        {
-                                            String er=""; String za="";
-
-
-                                            try
-                                            {
-                                                za= Double.toString(objects.get(i).getParseGeoPoint("positionAssocie").getLatitude());
-                                                er=za;
-                                                if(er!="")
-                                                {
-                                                    //Toast.makeText(getActivity(), "i"+i+"er" + er,Toast.LENGTH_LONG).show();
-                                                    myDataset.add(objects.get(i)) ;//tout ce qui ont une position gps
-                                                }
-
-
-
-
-                                            }
-
-                                            catch(Exception ez)
-                                            {
-                                                ez.printStackTrace();
-                                            }
-                                            //er=Double.toString(e.getLatitude()) ;
-
-
-
-                                        }
-
-                                        for(int z=0; z<myDataset.size();z++)
-
-                                        {
-                                            JSONArray label = myDataset.get(z).getJSONArray("labelResults");
-
-                                            try {
-                                                String  descri1 = label.getJSONObject(0).getString("description");
-                                                String  descri2 = label.getJSONObject(1).getString("description");
-
-
-
-                                                if(descri1.equals(description) ||descri1.equals(description2) || descri2.equals(description) ||descri2.equals(description2)  )
-                                                {
-                                                    //Toast.makeText(getActivity(), "meme description",Toast.LENGTH_LONG).show();
-                                                    getAllArticlesWithLabel.add(myDataset.get(z));
-
-                                                }
-
-                                            }catch (JSONException e1) {
-                                                e1.printStackTrace();
-                                            }
-
-
-
-                                        }
-
-
-
-                                    } else {
-                                        Log.d("maps", "Error: " + ex.getMessage());
-                                    }
-                                }
-                            });
-
-                            query1.setLimit(15);
-                        } else {
-                            Log.d("ParseException", "Error: " + e.getMessage() + "Code : " + e.getCode());
                         }
+                        else
+                            Log.d("ParseException", "Error: " + e.getMessage() + "Code : " + e.getCode());
+
                     }
                 });
-
-                /*----------------- FAIT TES MODIFICATIONS ICI -------------------*/
-
-
-if( getAllArticlesWithLabel.size()!=0)
-
-{
-    try {
-
-        CameraPosition googlePlex = CameraPosition.builder()
-                .target(new LatLng(getAllArticlesWithLabel.get(0).getParseGeoPoint("positionAssocie").getLatitude(), getAllArticlesWithLabel.get(0).getParseGeoPoint("positionAssocie").getLongitude()))
-                .zoom(10)
-                .bearing(0)
-                .tilt(45)
-                .build();
-
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
-        for (int zz = 0; zz < getAllArticlesWithLabel.size(); zz++) {
-            ParseFile file = getAllArticlesWithLabel.get(zz).getParseFile("photoIn");
-            // Bitmap bit;
-            ImageView e = new ImageView(getActivity());
-
-            Picasso.get()
-                    .load(file.getUrl())
-                    .into(e);
-            //  BitmapDrawable drawable = (BitmapDrawable) e.getDrawable();
-            byte[] bit = file.getData();
-            Bitmap bitmap = BitmapFactory.decodeByteArray(bit, 0, bit.length);
-            int height = 100;
-            int width = 100;
-            Bitmap smallMarker = Bitmap.createScaledBitmap(bitmap, width, height, false);
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(getAllArticlesWithLabel.get(zz).getParseGeoPoint("positionAssocie").getLatitude(), getAllArticlesWithLabel.get(zz).getParseGeoPoint("positionAssocie").getLongitude()))
-                    .icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
-                    .title(description+"/"+description2)
-            );
-
-        }
-    } catch (Exception ez) {
-        ez.printStackTrace();
-    }
-
-
-}
-else
-    {
-
-    CameraPosition googlePlex = CameraPosition.builder()
-            .target(new LatLng(43.4219999,7.0862462))
-            .zoom(10)
-            .bearing(0)
-            .tilt(45)
-            .build();
-    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
-}
-
-
-
-
-                /*----------------- FAIT TES MODIFICATIONS ICI -------------------*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-                /*--------------------------------------------------------------------------*/
 
             }
         });
 
-
         return rootView;
     }
 
-    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
-        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
-        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
-        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        vectorDrawable.draw(canvas);
-        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    private void fetchPinsData(final GoogleMap mMap){
+
+        final LatLngBounds.Builder builder = LatLngBounds.builder();
+        //builder.include(new LatLng(centerLat, centerLng));
+
+        for (final ElimPins pin: elimPins) {
+            builder.include(new LatLng(pin.getLatitude(), pin.getLongitude()));
+            Picasso.get().load(pin.getPictureURL()).into(new Target() {
+                @Override
+                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                    int height = 100; int width = 100;
+                    Bitmap smallMarker = Bitmap.createScaledBitmap(bitmap, width, height, false);
+                    mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(pin.getLatitude(), pin.getLongitude()))
+                            .icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+                            .title(pin.description));
+                }
+
+                @Override
+                public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+                    mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(pin.getLatitude(), pin.getLongitude()))
+                            .title(pin.description));
+                }
+
+                @Override
+                public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                }
+            });
+        }
+
+        LatLngBounds bounds = builder.build();
+
+        CameraUpdate update = CameraUpdateFactory.newLatLngBounds(bounds, 10);
+        mMap.animateCamera(update);
+
+/*
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(37.4629101,-122.2449094))
+                        .title("Iron Man")
+                        .snippet("His Talent : Plenty of money"));
+*/
     }
+
 }
+
+class ElimPins {
+
+    public String description;
+    public ParseGeoPoint geoPoint;
+    public ParseFile file;
+
+    ElimPins(){ description = "Description"; geoPoint = new ParseGeoPoint(0,0); file = null; }
+    ElimPins(String desc, ParseGeoPoint geo, ParseFile f){ description = desc; geoPoint = geo; file = f; }
+
+    public String getPictureURL() { return file.getUrl(); }
+    public Double getLatitude()   { return geoPoint.getLatitude(); }
+    public Double getLongitude()  { return geoPoint.getLongitude(); }
+}
+
